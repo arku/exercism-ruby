b676a5e916a04c1fd2dceee7af6b72edea962a38
Arun Kumar <arunkumarm1995@gmail.com>
2016-12-22 07:14:59 +0530

    Revert to a simple solution

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,33 +1,12 @@
 class Raindrops
-  attr_reader :number
-
-  def initialize(number)
-    @number = number
-  end
-
-  def convert
-    raindrop_speak = pling(number) + plang(number) + plong(number)
-    return number.to_s if raindrop_speak.empty?
-    raindrop_speak
-  end
-
   def self.convert(number)
-    new(number).convert
+    raindrop_speak = ''
+    raindrop_speak << 'Pling' if number % 3 == 0
+    raindrop_speak << 'Plang' if number % 5 == 0
+    raindrop_speak << 'Plong' if number % 7 == 0
+
+    raindrop_speak.empty? ? number.to_s : raindrop_speak
   end
-
-  def method_missing(method, *args)
-     raindrop_lang = { pling: 3, plang: 5, plong: 7 }
-
-    if raindrop_lang.has_key?(method)
-      if (args[0] % raindrop_lang[method] == 0)
-        speak = method.to_s.capitalize
-      end
-      return speak || ''
-    end
-
-    super
-  end
-
 end
 
 module BookKeeping
