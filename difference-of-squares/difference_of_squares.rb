539cf7c1bda6a6548222c1eda40e2f16071c9e1f
Arun Kumar <arunkumarm1995@gmail.com>
2016-12-23 06:49:15 +0530

    Initial commit for difference of squares problem

---
+++ difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,20 @@
+class Squares
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def square_of_sum
+  end
+
+  def sum_of_squares
+  end
+
+  def difference
+  end
+end
+
+module BookKeeping
+  VERSION = 3
+end
