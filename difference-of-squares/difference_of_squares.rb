cc16710345e08b1febcd92a4236e5bcdbf6f4ae8
Arun Kumar <arunkumarm1995@gmail.com>
2016-12-23 06:52:26 +0530

    Complete difference of squares problem

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -6,12 +6,21 @@ class Squares
   end
 
   def square_of_sum
+    square(number * (number + 1) / 2)
   end
 
   def sum_of_squares
+    (number * (number + 1) * (2 * number + 1)) / 6
   end
 
   def difference
+    square_of_sum - sum_of_squares
+  end
+
+  private
+
+  def square(num)
+    num * num
   end
 end
 
