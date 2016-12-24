9da8fd5a2032a93686a24225f038e9889f45e1f9
Arun Kumar <arunkumarm1995@gmail.com>
2016-12-24 06:54:03 +0530

    Complete pangram problem

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,8 +1,18 @@
+require 'set'
+
 class Pangram
-  def self.pangram?(phrase)    
+  def self.pangram?(phrase)
+    alphabets = Set.new('a'..'z')
+
+    phrase.each_char do |char|
+      alphabets.delete(char.downcase) if char.downcase.ord.between?(97, 122)
+      return true if alphabets.empty?
+    end
+    false
   end
 end
 
 module BookKeeping
   VERSION = 3
 end
+
