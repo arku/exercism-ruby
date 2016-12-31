884ca031715d065054f85fd047b121e8a4d90c13
Arun Kumar <arunkumarm1995@gmail.com>
2017-01-01 01:24:58 +0530

    Complete robot name challenge

--- robot-name/robot_name.rb
+++ robot-name/robot_name.rb
@@ -2,13 +2,30 @@ class Robot
   attr_reader :name
 
   def initialize
-    @name = nil
+    @name = random_name
   end
 
   def reset
+    @name = random_name
+  end
+
+  private
+
+  def random_name
+    srand # chooses a random seed based on time and process id
+    random_two_letters + random_three_digit_number.to_s
+  end
+
+  def random_two_letters
+    2.times.map { rand(65..90).chr }.join
+  end
+
+  def random_three_digit_number
+    rand(100..999)
   end
 end
 
 module BookKeeping
   VERSION = 2
 end
+
